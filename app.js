8d4e636d525d68f95e76a4ad7018342ecabc032c
L3p1989 <phogan110@live.com>
2021-02-15 21:41:11 -0700

    created displayPosition function, changed job variable to job4, created job1-3 objects. Called all job objects using displayPosition

--- app.js
+++ app.js
@@ -4,7 +4,25 @@ console.log(me.name.toUpperCase(), me.career, me.about)
 // Interests array)
 let interests = ["Fishing", "Camping", "Hiking", "Gaming", "Technology", "Coding"];
 console.log(interests);
-// work experience object
-let job = {name: "DirecTV", title: "Tech Support", description: "Answering phone calls for tech support on DirecTV related equipment and software issues. Controlling call flow by using scripts and related experience or training. Making every call a conversation and not a transaction. Meeting or exceeding goals put forth by my team leader"};
+// work experience objects
+let job1 = {name: "Pizza Hut", title: "Shift Lead", description: "Opening or closing store using proper procedure. Making drops and deposits. Doing daily inventory. Stocking store with new products when dropped off. Opening and closing drawers in the morning and at night. Assigning duties for team members to complete before leaving. Handling in sore and over the phone sales. Answering advanced customer inquiries or concerns. Making sure store is clean and all side activities have been completed by end of day."};
+
+let job2 = {name: "Pizza Hut", title: "Cook", description: "Taking orders by phone or in store. Keeping advanced product knowledge to quickly and efficiently make customer orders to perfect spec. prepping dough and products for use next day. Labeling prep with correct dates to ensure product quality. prepping orders for drivers to go out to customers. Taking cash or charge payments. Keeping the ingredient table in order and clean. Cleaning make table before open or after close."};
+
+let job3 = {name: "Pizza Hut", title: "Delivery Driver", description: "Taking calls as fast as possible to take customer orders, and inquiries. Driving to and from customer business' or homes as quick as possible. Always using the CARES model to ensure the highest level of customer service when in store or out of store. Handling cash for orders. Doing daily duties before leaving such as mopping store, cleaning and stocking bathrooms, cleaning oven racks, stocking chicken, cleaning and stocking wing table. Handling cash or charge sales."};
+
+let job4 = {name: "DirecTV", title: "Tech Support", description: "Answering phone calls for tech support on DirecTV related equipment and software issues. Controlling call flow by using scripts and related experience or training. Making every call a conversation and not a transaction. Meeting or exceeding goals put forth by my team leader"};
 // skills array
 let skills = ["Technical Support", "Customer Satisfaction", "Computer Repair", "Network Security", "Inventory Management", "Cash Handling", "BAM: HTML", "BAM: CSS", "BAM: Javascript"];
+// displayPosition function
+function displayPosition(name, title, description) {
+    console.log(name, title, description)
+};
+
+displayPosition(job1);
+
+displayPosition(job2);
+
+displayPosition(job3);
+
+displayPosition(job4);
