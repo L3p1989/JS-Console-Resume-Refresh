2068eddcb08bba323200ca4bbd19a9e07578d94f
L3p1989 <phogan110@live.com>
2021-02-15 21:10:47 -0700

    modified me console.log to capitilize my name

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 // Personal info object
 let me = {name: "L3p", career: "Aspiring Web Developer", about: "I've spent the greater majority of my life in customer service. I attempted to get into warehouse work but was injured in both jobs I tried. I am now pursuing my goal of becoming a Web developer"};
-console.log(me);
-// Interests array
+console.log(me.name.toUpperCase(), me.career, me.about)
+// Interests array)
 let interests = ["Fishing", "Camping", "Hiking", "Gaming", "Technology", "Coding"];
 console.log(interests);
 // work experience object
 let job = {name: "DirecTV", title: "Tech Support", description: "Answering phone calls for tech support on DirecTV related equipment and software issues. Controlling call flow by using scripts and related experience or training. Making every call a conversation and not a transaction. Meeting or exceeding goals put forth by my team leader"};
 // skills array
-let skills = ["Technical Support", "Customer Satisfaction", "Computer Repair", "Network Security", "Inventory Management", "Cash Handling", "HTML", "CSS", "Javascript"];
+let skills = ["Technical Support", "Customer Satisfaction", "Computer Repair", "Network Security", "Inventory Management", "Cash Handling", "BAM: HTML", "BAM: CSS", "BAM: Javascript"];
