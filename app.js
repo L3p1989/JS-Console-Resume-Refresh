1a8e94c50c5658e7f161626bf87738153606f6c7
L3p1989 <phogan110@live.com>
2021-02-15 20:51:47 -0700

    added console.logs for me, and interests

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 // Personal info object
-let me = {name: "L3p", career: "Aspiring Web Developer", about: "I've the greater majority of my life in customer service. I attempted to get into warehouse work but was injured in both jobs I tried. I am now pursuing my goal of becoming a Web developer"};
+let me = {name: "L3p", career: "Aspiring Web Developer", about: "I've spent the greater majority of my life in customer service. I attempted to get into warehouse work but was injured in both jobs I tried. I am now pursuing my goal of becoming a Web developer"};
+console.log(me);
 // Interests array
 let interests = ["Fishing", "Camping", "Hiking", "Gaming", "Technology", "Coding"];
+console.log(interests);
+// work experience
